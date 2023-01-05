2b6920fc42a17725b5e633b9c0cedf886038cab5
GoldenCredit <birongliu2003@gmail.com>
2023-01-05 11:24:57 -0500

    trying out to fix the device where you will see white space when rotated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from './components/common/Footer';
 
 export default function App() {
   return (
-    <div className='h-screen bg-white dark:bg-gray-900'>
+    <div className='h-screen w-full bg-white dark:bg-gray-900'>
       <Navagator />
       <Footer />
     </div>
