e0c6b30bf8ef9ce0b52cb793139ae76088251900
GoldenCredit <birongliu2003@gmail.com>
2023-01-05 12:35:40 -0500

    add theme to html tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from './components/common/Footer';
 
 export default function App() {
   return (
-    <div className='min-h-screen min-w-full bg-white dark:bg-gray-900'>
+    <div className='h-full max-w-md bg-white dark:bg-gray-900'>
       <Navagator />
       <Footer />
     </div>
