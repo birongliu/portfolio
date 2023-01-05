aac87f4fa5854f255f80306a22abea3cbdefa30a
GoldenCredit <birongliu2003@gmail.com>
2023-01-05 12:37:18 -0500

    revert back to old style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navagator from './components/common/Navbar';
 import Footer from './components/common/Footer';
 
 export default function App() {
   return (
-    <div className='h-full max-w-md bg-white dark:bg-gray-900'>
+    <div className='h-screen bg-white dark:bg-gray-900'>
       <Navagator />
       <Footer />
     </div>
