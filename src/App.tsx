9d1517d4e066cf88f32f71cd839782b029fdbefd
GoldenCredit <birongliu2003@gmail.com>
2023-01-05 11:12:37 -0500

    first commt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navagator from './components/common/Navbar';
+import Footer from './components/common/Footer';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen bg-white dark:bg-gray-900'>
+      <Navagator />
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
