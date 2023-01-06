9779057a2e2cbd97a03c3a2f629c272cdec95bc7
GoldenCredit <birongliu2003@gmail.com>
2023-01-06 12:05:48 -0500

    add

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { navLinks } from "../../utils/constants";
+export default function Footer() {
+	return (
+		<footer className="p-5 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900">
+			<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+				© {new Date().getFullYear()}{" "}
+				<a href="https://flowbite.com/" className="hover:underline">
+					Bi Rong Liu
+				</a>
+				. All Rights Reserved.
+			</span>
+			<ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+				{navLinks.map(link => (
+					<li key={link.name}>
+						<Link to={link.path} key={link.name} className="mr-4 hover:underline md:mr-6">
+							{link.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</footer>
+	);
+}
