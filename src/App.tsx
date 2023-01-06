b5426469e408f2965375cb8abb46cd6360ed8999
GoldenCredit <birongliu2003@gmail.com>
2023-01-06 12:24:11 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import Navagator from "./components/common/Navbar";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/Home/Home";
-import FourOFour from "./components/Error/404";
-import Footer from "./components/common/Footer";
+import { Home, Footer, FourOFour, Navigator } from "./components"
 
 export default function App() {
 	return (
 		<div className="dark:bg-gray-900">
-			<Navagator />
+			<Navigator />
 			<main>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/404" element={<FourOFour />} />
           <Route path="*" element={ <Navigate to="/404" replace />} />
+
 				</Routes>
 			</main>
       <Footer />
