6b77493c76b5d4d52402a73ec3f8953f1e1d05d9
GoldenCredit <birongliu2003@gmail.com>
2023-01-06 12:41:17 -0500

    remove header

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,11 @@ import FourOFour from "./Error/404"
 import Home from "./Home/Home"
 import Project from "./Project/Project"
 //import Footer from "./common/Footer"
-import Navigator from "./common/Navbar"
+//import Navigator from "./common/Navbar"
 export {
    FourOFour,
    Home,
    Project,
    //Footer,
-   Navigator
+   //Navigator
 }
