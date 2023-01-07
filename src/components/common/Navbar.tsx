64faf81006c763580b80bae8e738610cbc1b066c
GoldenCredit <birongliu2003@gmail.com>
2023-01-06 20:45:08 -0500

    hmm

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { LogoName, navLinks } from "../../utils/constants";
+import { LogoName } from "../../utils/constants";
 import Utils from "../../utils/Utils";
 import { Theme } from "../../utils/Types";
-import { NavLink } from "react-router-dom";
 
 function handleTheme(
 	event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -105,21 +104,30 @@ export default function Navigator() {
 					id="navbar-sticky"
 				>
 					<ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-					{navLinks.map((link) => (
-							<li key={link.name}>
-								<NavLink
-									to={link.path}
-									key={link.name}
-									className={({ isActive }) =>
-										isActive
-											? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-											: "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-70"
-									}
-								>
-									{link.name}
-								</NavLink>
-							</li>
-						))}
+						<li>
+							<a
+								href="/"
+								className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+							>
+								Home
+							</a>
+						</li>
+						<li>
+							<a
+								href="/about"
+								className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								About
+							</a>
+						</li>
+						<li>
+							<a
+								href="/"
+								className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								Services
+							</a>
+						</li>
 					</ul>
 				</div>
 			</div>
