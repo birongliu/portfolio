1715b8ec4f40f70466901ae69814da654ada1132
GoldenCredit <birongliu2003@gmail.com>
2023-01-08 14:16:53 -0500

    test

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,10 +1,16 @@
 import { Theme } from "./Types";
 
 export default class Utils {
-   static getCurrentTheme(): Theme {
+   public static getCurrentTheme(): Theme {
       const storageTheme = localStorage.getItem("theme") as Theme;
 		const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 		const currentTheme = storageTheme ? storageTheme : (systemTheme ? "dark" : "light");
       return currentTheme;
+   }
+   public static switchMobileTheme(currentTheme: Theme) {
+      const theme = (document.querySelector('meta[name="theme-color"]') as Element);
+      const currentColorTheme = currentTheme === "dark" ? "#11827" : "#cbd5e1";
+      theme.setAttribute("content", currentColorTheme);
+
    }
 }
