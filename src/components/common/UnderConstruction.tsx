4590380e9792f66f9b79be4f2e5e8cdd0c986e71
GoldenCredit <birongliu2003@gmail.com>
2023-01-07 22:12:04 -0500

    add under construction for about page

--- src/components/common/UnderConstruction.tsx
+++ src/components/common/UnderConstruction.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function UnderConstruction() {
 	return (
-		<main className="container flex items-center mx-auto px-20 flex-col gap-3 justify-center max-w-5xl min-h-screen py-20 whitespace-no-wrap sm:whitespace-normal">
-			<div className="relative bottom-20 text-center">
-				<h1 className=" dark:text-white font-bold text-3xl">
-					Under construction, check back soon!
+		<main className="flex items-center mx-auto px-8 flex-col gap-6 justify-center max-w-5xl min-h-screen">
+			<div className="relative text-center">
+				<h1 className="dark:text-white font-bold text-3xl">
+					🚧 Under construction 🚧
 				</h1>
-				<p className="dark:text-slate-400 text-slate-600">
-					This site is currently under construction. Please check back soon for
+				<p className="dark:text-slate-400 text-slate-600 font-bold">
+					This page is currently under construction. Please check back soon for
 					updates.
 				</p>
 			</div>
