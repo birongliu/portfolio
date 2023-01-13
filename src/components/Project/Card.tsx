21e464ef5a82254fbf47de0b460484d8ecb25a28
GoldenCredit <birongliu2003@gmail.com>
2023-01-13 11:58:57 -0500

    xs

--- src/components/Project/Card.tsx
+++ src/components/Project/Card.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ buildCard }: { buildCard: CardDataOptions[] }) {
 	return (
 		<ul className="gap-8 grid grid-cols-1 lg:grid-cols-3 place-items-stretch pt-12 sm:grid-cols-2 mb-10">
 			{buildCard.map((card) => (
 				<li className="animate-pop">
-					<section className="flex flex-col dark:bg-gray-800 h-full hover:-translate-y-2 hover:shadow-xl overflow-hidden relative rounded-xl shadow-md transition-[box-shadow,transform]">
+					<section className="flex flex-col dark:bg-slate-800 h-full hover:-translate-y-3 hover:shadow-4xl overflow-hidden relative rounded-xl shadow-md transition-[box-shadow,transform] last:right-2">
 						<div className="relative">
 							<img
 								key={card.title}
@@ -19,7 +19,10 @@ export default function Card() {
 						</div>
 						<div className="px-3 py-2">
 							{card.tags.map((tag) => (
-								<span key={tag} className="text-xs font-semibold inline-block py-1 px-2 rounded text-black bg-blue-400  last:mr-0 mr-1">
+								<span
+									key={tag}
+									className="text-xs font-semibold inline-block py-1 px-2 rounded text-black bg-blue-400  last:mr-0 mr-1"
+								>
 									#{tag}
 								</span>
 							))}
@@ -37,11 +40,12 @@ interface CardDataOptions {
 	tags: string[];
 }
 
-const buildCard: CardDataOptions[] = [
+export const buildCard: CardDataOptions[] = [
 	{
 		tags: ["NodeJS", "Prisma", "PostgreSQL"],
 		title: "Phoniex",
-		description: "A Discord bot for virtual economy, with features like earning currency, making transactions and interacting via commands, minigames and other features.",
+		description:
+			"A Discord bot for virtual economy, with features like earning currency, making transactions and interacting via commands, minigames and other features.",
 		image:
 			"https://cdn.discordapp.com/attachments/473528924027224075/1062826868899860531/Screenshot_2023-01-11_at_3.14.05_PM.png",
 	},
@@ -60,13 +64,3 @@ const buildCard: CardDataOptions[] = [
 			"https://cdn.discordapp.com/attachments/473528924027224075/1062826868899860531/Screenshot_2023-01-11_at_3.14.05_PM.png",
 	},
 ];
-/*
-export default function Card() {
-   return (
-   <main className="mx-auto px-8 max-w-5xl min-h-screen py-20">
-      <ul className='gap-8 grid grid-cols-1 lg:grid-cols-3 place-items-stretch pt-12 sm:grid-cols-2'>
-      </ul>
-   </main>
-   )
-
-}*/
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./Card";
+import Card, { buildCard } from "./Card";
 
 export default function Project() {
 	return (
@@ -27,7 +27,7 @@ export default function Project() {
 					<input
 						type="text"
 						id="simple-search"
-						className="rounded-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="dark:text-white rounded-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						placeholder="Search"
 						required
 					/>
@@ -53,7 +53,7 @@ export default function Project() {
 					<span className="sr-only">Search</span>
 				</button>
 			</form>
-			<Card />
+			<Card buildCard={buildCard} />
 		</main>
 	);
 }
