d3da02757eab186f918f208028c5200a2faa2f88
GoldenCredit <birongliu2003@gmail.com>
2023-01-19 17:32:39 -0500

    update color

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -30,7 +30,7 @@ export default function Project() {
 					<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
 						<svg
 							aria-hidden="true"
-							className="w-5 h-5 text-gray-500 dark:text-gray-400"
+							className="w-5 h-5 text-gray-500 dark:text-gray-500"
 							fill="currentColor"
 							viewBox="0 0 20 20"
 							xmlns="http://www.w3.org/2000/svg"
@@ -45,13 +45,14 @@ export default function Project() {
 					<input
 						type="text"
 						id="simple-search"
-						className="dark:text-white rounded-xl bg-gray-50 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+						className="dark:text-white  rounded-xl bg-slate-200 border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						placeholder="Search"
 						required
 					/>
 				</div>
 				<button
 					type="submit"
+					onClick={e => e.preventDefault()}
 					className="rounded-full p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 				>
 					<svg
