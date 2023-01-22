9221522a08a1112acf0b889f1efef8895919d26e
GoldenCredit <birongliu2003@gmail.com>
2023-01-22 13:34:32 -0500

    add project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -82,27 +82,7 @@ export default function Project() {
 						required
 					/>
 				</div>
-				<button
-					form="searchForm"
-					type="submit"
-					className="rounded-full p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>
-					<svg
-						className="w-5 h-5"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						></path>
-					</svg>
-					<span className="sr-only">Search</span>
-				</button>
+				
 			</form>
 			{project && project.length ? (
 				<Card data={resolveToCard(project)} />
