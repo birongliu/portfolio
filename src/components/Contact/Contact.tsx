bbcbdd1902209626d93f100d26b0b50bde4c0e2b
GoldenCredit <birongliu2003@gmail.com>
2023-01-25 13:09:47 -0500

    under construction contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,6 @@
 import React from "react";
+import UnderConstruction from "../common/UnderConstruction";
 
 export default function Contact() {
-	return (
-		<main className="flex items-center px-8 flex-col justify-center max-w-5xl min-h-screen py-20">
-
-		</main>
-	);
+	return <UnderConstruction />
 }
