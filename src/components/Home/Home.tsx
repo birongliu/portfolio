83c35c668726d468b538faeba17979faa2302866
GoldenCredit <birongliu2003@gmail.com>
2023-02-16 15:35:10 -0500

    lazing loading

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,7 @@ export default function Home() {
 		<main className="flex items-center mx-auto px-8 flex-col gap-6 justify-center max-w-5xl min-h-screen py-20">
 			<div className="grid place-items-center">
 			<img
+					loading="lazy"
 					className="w-[11rem] rounded-full p-[4px] bg-gradient-to-r from-green-400 to-blue-500 dark:from-orange-500 dark:to-blue-400"
 					alt="profile"
 					src="https://cdn.discordapp.com/avatars/358382764959203338/d175e81df15d96edfc1d68a067e4a138.webp?size=512"
