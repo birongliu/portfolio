31024820dd4aac72bb3f56ed9b4766d7a69d285b
GoldenCredit <birongliu2003@gmail.com>
2023-06-02 15:32:58 -0400

    different theme for mobile navlinks

--- src/components/common/Header/NavLinks.tsx
+++ src/components/common/Header/NavLinks.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { navLinks } from "../../../utils/constants";
 import { NavLink } from "react-router-dom";
 
-export default function NavLinks({ setNavigate }: { setNavigate: React.Dispatch<React.SetStateAction<boolean>> }) {
+export default function NavLinks({ setNavigate, isMobile }: { isMobile: boolean, setNavigate: React.Dispatch<React.SetStateAction<boolean>> }) {
 	return (
 		<>
 			{navLinks.map((link) => (
@@ -12,10 +12,15 @@ export default function NavLinks({ setNavigate }: { setNavigate: React.Dispatch<
 					key={link.name}
 					className={({ isActive }) =>
 						`${
+							!isMobile ? 
 							isActive
 								? "font-medium text-lg dark:hover:text-sky-300 hover:text-blue-900 px-2 text-center dark:text-sky-200 text-blue-900"
 								: "font-medium text-lg dark:hover:text-sky-200 hover:text-sky-900 px-2 text-center dark:text-slate-400 text-slate-900"
-						}`
+							: isActive 
+							? "font-medium text-lg dark:hover:text-sky-300 hover:text-blue-900 px-2 text-center dark:text-sky-500 text-blue-900"
+							: "font-medium text-lg dark:hover:text-sky-200 hover:text-sky-900 px-2 text-center dark:text-blue-600 text-blue-700"
+
+							}`
 					}
 				>
 					{link.name}
