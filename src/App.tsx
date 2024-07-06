f0cd96cc83a52a6f17f88aa87032de53ae8d5e30
birongliu <birongliu2003@gmail.com>
2024-07-06 19:27:31 -0400

    refactor: Update App.css and add App.tsx with header component and initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <p>Under Construction</p>
+    </Fragment>
+  );
+}
+
+export default App;
