44b783424f3f2534b380f7a81665afeddb3d2d79
birongliu <birongliu2003@gmail.com>
2024-07-06 19:23:40 -0400

    chore: Add header component and styles for navigation menu and theme icon

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./Header.css";
+import menu_icon from "../../assets/menu-svgrepo-com.svg";
+import x from "../../assets/close-circle-svgrepo-com.svg";
+import theme_sun from "../../assets/sun.svg"
+import theme_moon from "../../assets/moon.svg"
+
+
+export default function Header() {
+  const [theme, setTheme] = useState(true);
+  const [menu, setMenu] = useState(false);
+  return (
+  <header className="header">
+    <div className="header-container">
+    <div className="logo-container">
+        <span className="logo">birong.liu</span>
+      </div>
+      <ul className="nav-container">
+        <li>About</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+      <div className="theme-menu-container">
+        <img className="theme-icon" onClick={() => setTheme((theme) => !theme)} height={30} width={30} src={theme ? theme_sun : theme_moon} alt="theme-icon" />
+        <img className="menu-icon" onClick={() => setMenu((menu) => !menu)} height={30} width={30} src={!menu ? menu_icon : x} alt="Menu" />
+      </div>
+    </div>
+    <ul className={`mobile-nav-${menu ? "open" : "close"}`}>
+        <li>About</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+  </header>
+  )
+}
