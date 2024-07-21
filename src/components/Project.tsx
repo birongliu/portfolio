299b0ffe457bada8a3dc4abb5806decbfa0ce82a
birongliu <birongliu2003@gmail.com>
2024-07-21 18:31:06 -0400

    refactor: Update Project component to include GridCard for displaying data

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import GridCard, { data } from './ui/GridCard'
 
 export default function Project() {
   return (
-    <section id="project" className="h-screen py-20">
+    <section id="project" className="h-full py-20">
     <div className="text-white-100 font-bold text-5xl flex justify-center">
       Project
     </div>
+    <GridCard items={[data]} />
   </section>
   )
 }
