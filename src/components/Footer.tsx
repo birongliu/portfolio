1bbdb03e4a1f5644ca4cdde3f04729ca925cc045
birongliu <birongliu2003@gmail.com>
2024-07-22 13:34:07 -0400

    refactor: Update Contact component styles and add email link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='py-5'>
+    <footer className='py-10'>
         <div className='text-white-100 flex justify-center gap-1'> <span className='font-bold text-gray-400'>© All Rights Reserved By</span> birongliu.</div>
     </footer>
   )
