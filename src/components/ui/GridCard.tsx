fd637b15929a4cc63408ff69452aaf9574a30341
birongliu <birongliu2003@gmail.com>
2024-07-21 20:45:18 -0400

    refactor: Update GridCard component to display images and descriptions in a responsive grid layout

--- src/components/ui/GridCard.tsx
+++ src/components/ui/GridCard.tsx
@@ -2,11 +2,10 @@ import { GridItem } from "../../lib/types";
 
 export default function Grid({ items }: { items: GridItem[] }) {
   return (
-    <div className="grid py-10 grid-cols-1 gap-5 md:grid-cols-3">
+    <div className="grid py-10 grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
       {items.map((item) => (
-        <div className="text-white bg-black-200 hover:bg-black-300 rounded">
+        <div className="text-white bg-black-300 hover:bg-black-200 rounded p-5">
           <img src={item.photo} className="rounded w-full" alt={item.title} />
-
           <div className="p-2">
             <h1 className="text-white-100 font-bold text-2xl">{item.title}</h1>
             <p className="text-white-100 text-base mb-4 text-wrap">{item.description}</p>
@@ -21,7 +20,7 @@ export default function Grid({ items }: { items: GridItem[] }) {
                   />
                 ))}
               </div>
-              <div>
+              <div className="flex gap-2">
                 {item.action.map((action) => (
                   <a href={action.url}>
                     <img
