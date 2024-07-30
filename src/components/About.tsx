0900bea159b630efd75a557805354566cdf2b01d
birongliu <birongliu2003@gmail.com>
2024-07-30 12:38:51 -0400

    refactor: Update About component with optimized technology section styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,38 +1,59 @@
-import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import { techStack } from "../lib/constants";
 export default function About() {
   return (
-    <section id="about" className="py-40 h-full px-5">
+    <section id="about" className="py-48 h-full px-5">
       <div className="grid lg:grid-cols-2 gap-2 text-gray-50">
         <div className="order-2 lg:order-none">
           <div className="flex items-center gap-2">
             <hr className="py-2 w-20 bg-white-100 border-none rounded-sm" />
-            <h2 className="font-bold text-2xl text-blue-200 uppercase">About Me</h2>
+            <h2 className="font-bold text-2xl text-blue-200 uppercase">
+              About Me
+            </h2>
           </div>
-          <p>
-            I am passionate about web development and have a strong foundation
-            in HTML, CSS, and JavaScript. I enjoy creating dynamic and
-            responsive web applications that provide a great user experience.
+          <p className="mb-5 md:text-base font-semibold">
+            I am passionate about creating Web Apps with UI/UX User Interface, I
+            specialize in a wide range of programming languages and
+            technologies. My experience includes developing full-stack web
+            applications, where I've honed my skills in using React,
+            Express, and PostgreSQL, among others.
           </p>
+          <a
+            href="/birongliu-resume.pdf"
+            aria-label="resume"
+            download="birongliu-resume.pdf"
+          >
+            <button className="border bg-primary border-cyan-900 p-2 rounded-xl hover:cursor-pointer hover:bg-cyan-800 text-white-200">
+              Download Resume
+            </button>
+          </a>
           <div className="flex items-center gap-2 pt-10 pb-2">
-            <hr className="py-2 w-20 bg-white-100 border-none rounded-sm" />
-            <h2 className="font-bold text-2xl text-blue-200 uppercase">Technology</h2>
+            <hr className="py-2 w-20 bg-white-100 border-none rounded" />
+            <h2 className="font-bold text-2xl text-blue-200 uppercase">
+              Technology
+            </h2>
           </div>
-          <AnimatePresence mode="wait">
-          <motion.div whileHover={{ height: "8rem" }} className="border-cyan-900 border bg-primary h-10 rounded w-full">
-            <ul className="flex gap-1 py-2 px-2">
-              {techStack.map(stack => (
-                <li><img src={stack.image} alt={stack.name} width={25} height={25} /></li>
+          <div className="border-cyan-900 border bg-primary h-fit rounded w-fit">
+            <ul className="flex gap-1 py-2 px-2 items-center">
+              {techStack.map((stack) => (
+                <>
+                  <span className="text-base flex border h-5 rounded-xl first:hidden last:hidden border-cyan-900" />
+                  <li>
+                    <img
+                      src={stack.image}
+                      alt={stack.name}
+                      width={25}
+                      height={25}
+                    />
+                  </li>
+                </>
               ))}
             </ul>
-
-          </motion.div>
-          </AnimatePresence>
+          </div>
         </div>
         <img
-          src="/projects/empowerED.png"
-          className="object-cover rounded w-full h-full"
+          src="https://assets.about.me/background/users/b/i/r/birongliu_1598379907_157.jpg"
+          className="w-full object-cover rounded"
           alt="birong-picture"
         />
       </div>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 import { GridItem, NavItemProps, TechStack } from "./types";
 
-export const data: GridItem[] = [{
+export const data: GridItem[] = [
+  {
     photo: "./projects/empowerED.png",
     category: "web dev",
     title: "EmpowerED",
@@ -22,18 +23,20 @@ export const data: GridItem[] = [{
           "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
       },
     ],
-    action: [{
-      name: "github",
-      url: "https://github.com/pranitha05/EmpowerED",
-      src: "./icons/github.png"
-    },
-    {
-      name: "website",
-      url: "https://google.com",
-      src: './icons/globe.svg'
-    }],
+    action: [
+      {
+        name: "github",
+        url: "https://github.com/pranitha05/EmpowerED",
+        src: "./icons/github.png",
+      },
+      {
+        name: "website",
+        url: "https://google.com",
+        src: "./icons/globe.svg",
+      },
+    ],
   },
-  ];
+];
 
 export const techStack: TechStack[] = [
   {
@@ -42,11 +45,6 @@ export const techStack: TechStack[] = [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg",
   },
   {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-  {
     name: "JS",
     image:
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
@@ -57,80 +55,26 @@ export const techStack: TechStack[] = [
       "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
   },
   {
-    name: "JS",
+    name: "NodeJS",
     image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1180px-Node.js_logo.svg.png?20170401104355",
   },
   {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-  {
-    name: "JS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  },
-  {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-  {
-    name: "JS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  },
-  {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-  {
-    name: "JS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  },
-  {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-  {
-    name: "JS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  },
-  {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-  {
-    name: "JS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  },  {
-    name: "CSS",
-    image:
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg",
-  },
-
-]
+    name: "MongoDB",
+    image: "https://storage-us-gcs.bfldr.com/h5psv9c3jbk88pwc3xn79pp/v/1069931061/original/MongoDB_SpringGreen.png?Expires=1722435605&KeyName=gcs-bfldr-prod&Signature=Jb70gUvUzP_mO5Ww4Lj9ovO_0Os="
+  }
+];
 export const navItems: NavItemProps[] = [
-    {
-      name: "About",
-      link: "#about",
-    },
-    {
-      name: "Project",
-      link: "#project",
-    },
-    {
-      name: "Contact",
-      link: "#contact",
-    },
-  ];
-  
-
-
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Project",
+    link: "#project",
+  },
+  {
+    name: "Contact",
+    link: "#contact",
+  },
+];
